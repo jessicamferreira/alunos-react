4732b4623ce7fc98c561b780c6c8bcfdf859dd5f
Jean Terhorst <acebola08@gmail.com>
2022-08-17 20:06:07 -0300

    comit

--- src/config/layout/Default.tsx
+++ src/config/layout/Default.tsx
@@ -161,6 +161,16 @@ const LayoutDefault: React.FC<LayoutDefaultPros> = ({
               </ListItemButton>
             </ListItem>
           </Link>
+          <Link to="/produtos">
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <UserIcon />
+                </ListItemIcon>
+                <ListItemText primary="Produtos" />
+              </ListItemButton>
+            </ListItem>
+          </Link>
           <Link to="/about">
             <ListItem disablePadding>
               <ListItemButton>
---
+++ src/pages/produtos/Produtos.tsx
@@ -0,0 +1,63 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, TextField, Typography } from '@mui/material';
+import React from 'react';
+
+export const Produtos: React.FC = () =>{
+  return(
+    <React.Fragment>
+    <Typography variant='h3'>Produtos {}</Typography>
+    <Button  variant='outlined'>Cadastrar</Button>
+    <Button  variant='outlined'>Disparar</Button>
+    <Button  variant='outlined'>Limpar</Button>
+{/* 
+    {.map(item => {
+      return (
+        <div key={item.matricula} className="mt-5">
+          <Typography>Nome: {item.nome}</Typography>
+          <Typography>Idade: {item.idade}</Typography>
+          <Typography>Matricula: {item.matricula}</Typography>
+        </div>
+      )
+    })} */}
+    {/* <div>
+
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        Cadastrar aluno
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Para cadastrar um aluno, utilize o fomrulário abaixo.
+        </DialogContentText>
+        <Grid container spacing={2} className="mt-5">
+          <Grid item xs={12}>
+            <TextField value={nome} onChange={(e) => setNome(e.target.value)} label="Digite o nome" fullWidth color="secondary" focused />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField value={idade} onChange={(e) => setIdade(e.target.value)} label="Idade" fullWidth color="secondary" />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField value={matricula} onChange={(e) => setMatricula(e.target.value)} label="Matricula" fullWidth color="secondary" />
+          </Grid>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancelar</Button>
+        <Button variant='contained' onClick={()=> salvarNomeNoRedux()}>
+          Salvar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </div> */}
+  </React.Fragment>
+  
+
+
+  )
+
+
+}
