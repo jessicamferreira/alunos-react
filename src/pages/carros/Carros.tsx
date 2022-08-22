3d4313ba57b7b349f9e41f6cde8401e67894dd86
Matheus Konig <matheuslkonig@gmail.com>
2022-08-22 20:17:17 -0300

    Testando adicionar um carro.

--- src/pages/carros/Carros.tsx
+++ src/pages/carros/Carros.tsx
@@ -1,6 +1,11 @@
-import { Button, Grid, Typography } from "@mui/material"
-
-export const Carros: React.FC = () => {
+import { Button, Grid, Typography } from "@mui/material";
+import { useAppDispatch } from '../../store/hooks';
+import { addCarro } from '../../store/modules/carros/carrosSlice';
+export const Carros = () => {
+  const dispatch = useAppDispatch();
+  const addCarro1 = () => {
+    dispatch(addCarro({marca: "Ford", modelo: "Fusion Titanium", cor: "branco pérola"}));
+  }
   return(
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -9,7 +14,7 @@ export const Carros: React.FC = () => {
         </Typography>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <Button variant='outlined'>Adicionar</Button>
+        <Button variant='outlined' onClick={addCarro1}>Adicionar</Button>
         <Button variant='outlined'>Adicionar vários</Button>
         <Button variant='outlined'>Excluir todos</Button>
       </Grid>
--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -3,9 +3,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import books from "./books/BooksSlice";
 import items from "./items/ItemsSlice";
 import aluno from "./alunos/AlunoSlice";
+import carros from "./carros/carrosSlice";
 
 export default combineReducers({
   books,
   items,
   aluno,
+  carros
 });
