55a4781014e9fa41c35a3bdf572c85441730f3b4
BuildTools <unconfigured@null.spigotmc.org>
2022-08-22 20:28:09 -0300

    addMany-removeAll

--- src/pages/carros/Carros.tsx
+++ src/pages/carros/Carros.tsx
@@ -1,12 +1,34 @@
 import { Button, Grid, Typography } from "@mui/material";
+
 import { useAppDispatch } from '../../store/hooks';
-import { addCarro } from '../../store/modules/carros/carrosSlice';
+
+import { addCarro, addManyCarros, deleteAll } from '../../store/modules/carros/carrosSlice';
+
+
+
 export const Carros = () => {
+
   const dispatch = useAppDispatch();
+
   const addCarro1 = () => {
     dispatch(addCarro({marca: "Ford", modelo: "Fusion Titanium", cor: "branco pérola"}));
   }
+
+  const addManyCarros12 = () => {
+    const carros = addManyCarros([
+      { marca: "Ford", cor: "Vermelho", modelo: "Fiesta" },
+      { marca: "Chevrolet", cor: "Branco", modelo: "Onix" },
+      { marca: "Ford", cor: "Verde", modelo: "K" },
+    ])
+  }
+
+  const removeMany = () => {
+    const carros = dispatch(deleteAll())
+    console.log(carros)
+  }
+
   return(
+
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h3" color="primary">
@@ -15,9 +37,10 @@ export const Carros = () => {
       </Grid>
       <Grid item xs={12} sm={6}>
         <Button variant='outlined' onClick={addCarro1}>Adicionar</Button>
-        <Button variant='outlined'>Adicionar vários</Button>
-        <Button variant='outlined'>Excluir todos</Button>
+        <Button variant='outlined' onClick={addManyCarros12}>Adicionar vários</Button>
+        <Button variant='outlined' onClick={removeMany}>Excluir todos</Button>
       </Grid>
     </Grid>
+
   )
 }
